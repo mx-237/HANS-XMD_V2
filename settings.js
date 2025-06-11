a6718ae3ff7ac164ae842cf3949f810522460ec5
mx-237 <filsrevelateur@gmail.com>
2025-06-11 16:10:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,14 +7,14 @@
 const fs = require('fs')
 const chalk = require('chalk')
 //contact details
-global.ownernomer = "255756530143"
+global.ownernomer = "237621703294"
 global.ownername = "HANSTZ"
 global.ytname = "HANSTZ-TECH"
 global.socialm = "GitHub: Mrhanstz"
 global.location = "AFRICA, TANZANIA, DODOMA"
-global.ownernumber = '255756530143'  //creator number
+global.ownernumber = '237621703294'  //creator number
 global.ownername = 'HANSTZ' //owner name
-global.botname = 'HANS-XMD' //name of the bot
+global.botname = '𝐒ᵃˡ乇 в𝓪үᗴ' //name of the bot
 //sticker details
 global.packname = 'Sticker By'
 global.author = 'HANS-XMD'
